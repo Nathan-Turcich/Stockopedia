342f4e5a196b792e5458b1d11880db938f6b7cba
Nathan Turcich <nmt2@illinois.edu>
2019-03-12 22:38:10 -0500

    Hello

--- Stockopedia/Back End/service.php
+++ Stockopedia/Back End/service.php
@@ -2,29 +2,26 @@
 
 // Create connection
 $con=mysqli_connect("sp19-cs411-49.cs.illinois.edu","root","374sucks","Stockopedia");
- 
+ //Hello
 // Check connection
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()){
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
  
-// This SQL statement selects ALL from the table 'Locations'
+// This SQL statement selects ALL from the table 'hstocks'
 $sql = "SELECT name 
 FROM hstocks
 GROUP BY name";
  
 // Check if there are results
-if ($result = mysqli_query($con, $sql))
-{
+if ($result = mysqli_query($con, $sql)){
 	// If so, then create a results array and a temporary one
 	// to hold the data
 	$resultArray = array();
 	$tempArray = array();
  
 	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
+	while($row = $result->fetch_object()){
 		// Add each row into our results array
 		$tempArray = $row;
 	    array_push($resultArray, $tempArray);
