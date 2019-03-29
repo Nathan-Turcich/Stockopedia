7df4f22ee08348fbe551f9eb2112a3459c0a76f2
Nathan Turcich <nmt2@illinois.edu>
2019-03-28 23:10:54 -0500

    Added View

---
+++ Stockopedia/Login & Signup/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  Stockopedia
+//
+//  Created by Nathan Turcich on 3/28/19.
+//  Copyright © 2019 Joey Chung. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    //MARK: - Variables
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    //MARK: - Views Appearing
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeBorder()
+    }
+    
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func cancelButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - Helper Functions
+    fileprivate func makeBorder() {
+        usernameTextField.layer.borderColor = UIColor.black.cgColor
+        usernameTextField.layer.borderWidth = 1
+        passwordTextField.layer.borderColor = UIColor.black.cgColor
+        passwordTextField.layer.borderWidth = 1
+        signUpButton.layer.borderColor = UIColor.black.cgColor
+        signUpButton.layer.borderWidth = 1
+    }
+}
