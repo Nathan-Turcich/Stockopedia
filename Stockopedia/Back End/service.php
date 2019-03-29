4a110dc9d214e5041a23f5dec5b40dd57b14537c
Nathan Turcich <nmt2@illinois.edu>
2019-03-29 16:15:32 -0500

    hello

--- Stockopedia/Back End/service.php
+++ Stockopedia/Back End/service.php
@@ -9,32 +9,7 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     
-    if($_GET["query"] === "get_all_stocks") {
-        // This SQL statement selects ALL from the table 'Locations'
-        $sql = "SELECT name
-        FROM hstocks
-        GROUP BY name";
-        
-        // Check if there are results
-        if ($result = mysqli_query($con, $sql))
-        {
-            // If so, then create a results array and a temporary one
-            // to hold the data
-            $resultArray = array();
-            $tempArray = array();
-            
-            // Loop through each row in the result set
-            while($row = $result->fetch_object())
-            {
-                // Add each row into our results array
-                $tempArray = $row;
-                array_push($resultArray, $tempArray);
-            }
-            
-            // Finally, encode the array to JSON and output the results
-            echo json_encode($resultArray);
-        }
-    }else if($_GET["query"] === "test") {
+    if($_GET["query"] === "test") {
         // This SQL statement selects ALL from the table 'Locations'
         $sql = "SELECT name
         FROM hstocks
@@ -58,6 +33,32 @@
             
             // Finally, encode the array to JSON and output the results
             echo json_encode($resultArray);
+        }
+        else{
+            // This SQL statement selects ALL from the table 'Locations'
+            $sql = "SELECT name
+            FROM hstocks
+            GROUP BY name";
+            
+            // Check if there are results
+            if ($result = mysqli_query($con, $sql))
+            {
+                // If so, then create a results array and a temporary one
+                // to hold the data
+                $resultArray = array();
+                $tempArray = array();
+                
+                // Loop through each row in the result set
+                while($row = $result->fetch_object())
+                {
+                    // Add each row into our results array
+                    $tempArray = $row;
+                    array_push($resultArray, $tempArray);
+                }
+                
+                // Finally, encode the array to JSON and output the results
+                echo json_encode($resultArray);
+            }
         }
     }
     
