aff6a473b27edc06704f7aedd2b981e4f0884e3a
Joey <jnchung2@illinois.edu>
2019-03-29 16:12:55 -0500

    If Statements Only

--- Stockopedia/Back End/service.php
+++ Stockopedia/Back End/service.php
@@ -9,29 +9,56 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     
-    // This SQL statement selects ALL from the table 'Locations'
-    $sql = "SELECT name
-    FROM hstocks
-    GROUP BY name";
-    
-    // Check if there are results
-    if ($result = mysqli_query($con, $sql))
-    {
-        // If so, then create a results array and a temporary one
-        // to hold the data
-        $resultArray = array();
-        $tempArray = array();
+    if($_GET["query"] === "get_all_stocks") {
+        // This SQL statement selects ALL from the table 'Locations'
+        $sql = "SELECT name
+        FROM hstocks
+        GROUP BY name";
         
-        // Loop through each row in the result set
-        while($row = $result->fetch_object())
+        // Check if there are results
+        if ($result = mysqli_query($con, $sql))
         {
-            // Add each row into our results array
-            $tempArray = $row;
-            array_push($resultArray, $tempArray);
+            // If so, then create a results array and a temporary one
+            // to hold the data
+            $resultArray = array();
+            $tempArray = array();
+            
+            // Loop through each row in the result set
+            while($row = $result->fetch_object())
+            {
+                // Add each row into our results array
+                $tempArray = $row;
+                array_push($resultArray, $tempArray);
+            }
+            
+            // Finally, encode the array to JSON and output the results
+            echo json_encode($resultArray);
         }
+    }else if($_GET["query"] === "test") {
+        // This SQL statement selects ALL from the table 'Locations'
+        $sql = "SELECT name
+        FROM hstocks
+        WHERE name = "AAPL"";
         
-        // Finally, encode the array to JSON and output the results
-        echo json_encode($resultArray);
+        // Check if there are results
+        if ($result = mysqli_query($con, $sql))
+        {
+            // If so, then create a results array and a temporary one
+            // to hold the data
+            $resultArray = array();
+            $tempArray = array();
+            
+            // Loop through each row in the result set
+            while($row = $result->fetch_object())
+            {
+                // Add each row into our results array
+                $tempArray = $row;
+                array_push($resultArray, $tempArray);
+            }
+            
+            // Finally, encode the array to JSON and output the results
+            echo json_encode($resultArray);
+        }
     }
     
     // Close connections
