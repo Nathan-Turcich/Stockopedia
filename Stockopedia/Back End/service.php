51e7233c1ec16f9c1f0f1c2045de9f6cf0adf062
Joey <jnchung2@illinois.edu>
2019-03-29 16:32:55 -0500

    New Method

--- Stockopedia/Back End/service.php
+++ Stockopedia/Back End/service.php
@@ -9,65 +9,35 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     
-    function test_query() {
-        // This SQL statement selects ALL from the table 'Locations'
+    $sql = ""
+    if($_GET["query"] === "test") {
         $sql = "SELECT name
         FROM hstocks
         WHERE name = 'AAPL'";
-        
-        // Check if there are results
-        if ($result = mysqli_query($con, $sql))
-        {
-            // If so, then create a results array and a temporary one
-            // to hold the data
-            $resultArray = array();
-            $tempArray = array();
-            
-            // Loop through each row in the result set
-            while($row = $result->fetch_object())
-            {
-                // Add each row into our results array
-                $tempArray = $row;
-                array_push($resultArray, $tempArray);
-            }
-            
-            // Finally, encode the array to JSON and output the results
-            echo json_encode($resultArray);
-        }
-    }
-    
-    function get_all_stocks_query() {
-        // This SQL statement selects ALL from the table 'Locations'
+    }else if($_GET["query"] === "get_all_stocks"){
         $sql = "SELECT name
         FROM hstocks
         GROUP BY name";
-        
-        // Check if there are results
-        if ($result = mysqli_query($con, $sql))
-        {
-            // If so, then create a results array and a temporary one
-            // to hold the data
-            $resultArray = array();
-            $tempArray = array();
-            
-            // Loop through each row in the result set
-            while($row = $result->fetch_object())
-            {
-                // Add each row into our results array
-                $tempArray = $row;
-                array_push($resultArray, $tempArray);
-            }
-            
-            // Finally, encode the array to JSON and output the results
-            echo json_encode($resultArray);
-        }
     }
     
-    if($_GET["query"] === "test") {
-        test_query();
-    }
-    else{
-        get_all_stocks_query();
+    // Check if there are results
+    if ($result = mysqli_query($con, $sql))
+    {
+        // If so, then create a results array and a temporary one
+        // to hold the data
+        $resultArray = array();
+        $tempArray = array();
+        
+        // Loop through each row in the result set
+        while($row = $result->fetch_object())
+        {
+            // Add each row into our results array
+            $tempArray = $row;
+            array_push($resultArray, $tempArray);
+        }
+        
+        // Finally, encode the array to JSON and output the results
+        echo json_encode($resultArray);
     }
     
     // Close connections
