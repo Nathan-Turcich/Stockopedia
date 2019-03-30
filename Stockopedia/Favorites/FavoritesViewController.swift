ae9d926e70e169a68cfeeb60be84ba5e3e2ba0a3
Nathan Turcich <nmt2@illinois.edu>
2019-03-29 21:58:59 -0500

    Fixed Favorites

--- Stockopedia/Favorites/FavoritesViewController.swift
+++ Stockopedia/Favorites/FavoritesViewController.swift
@@ -8,27 +8,44 @@
 
 import UIKit
 
-var favoritesList: [String] = []
 
 class FavoritesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     //MARK: - Variables
     @IBOutlet var tableView: UITableView!
     @IBOutlet weak var noFavoritesLabel: UILabel!
+    let activityIndicator = UIActivityIndicatorView()
+    var favoritesList: [String] = []
     
     //MARK: - Views Appearing
     override func viewDidLoad() {
         super.viewDidLoad()
         Utils.setBars(navBar: (navigationController?.navigationBar)!, tabBar: (tabBarController?.tabBar)!)
+        loadingStarted()
+        DownloadData.downloadFavoritedList(completion: { (list) in
+            if let favs = list {
+                if list?.count == 0 {
+                    self.tableView.isHidden = true
+                    self.activityIndicator.stopAnimating()
+                    self.noFavoritesLabel.isHidden = false
+                }
+                else{
+                    DispatchQueue.main.async {
+                        self.favoritesList = favs
+                        self.tableView.reloadData()
+                        self.tableView.isHidden = false
+                        self.tableView.isScrollEnabled = true
+                        self.tableView.separatorStyle = .singleLine
+                        self.tableView.allowsSelection = true
+                        self.noFavoritesLabel.isHidden = true
+                        self.activityIndicator.stopAnimating()
+                    }
+                }
+            }
+            else { print("Error getting data") }
+        })
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        favoritesList = UserDefaults.standard.stringArray(forKey: "FavoriteList") ?? [""]
-        enableDisableTableView()
-    }
-    
     override var preferredStatusBarStyle: UIStatusBarStyle{ return .lightContent }
-
     
     //MARK: - Tableview Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return favoritesList.count }
@@ -36,38 +53,29 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:FavoritesTableViewCell = tableView.dequeueReusableCell(withIdentifier: "favoritesCell") as! FavoritesTableViewCell
         cell.favoritesStockLabel.text = favoritesList[indexPath.row]
-        cell.favoritesStockLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        cell.favoritesStockLabel.numberOfLines = 1
-        cell.favoritesStockLabel.textAlignment = .left
         return cell
     }
     
     //Delete Cells
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            if let index = favoritesList.index(of: favoritesList[indexPath.row]) { favoritesList.remove(at: index) }
-            if favoritesList.count == 0 { favoritesList.append("")}
-            UserDefaults.standard.set(favoritesList, forKey: "FavoriteList")
-            enableDisableTableView()
+            
         }
     }
     
-    func enableDisableTableView(){
-        if favoritesList[0] != "" {
-            if favoritesList[0] == "" { favoritesList.remove(at: 0) }
-            tableView.isHidden = false
-            tableView.isScrollEnabled = true
-            tableView.rowHeight = 30
-            tableView.separatorStyle = .singleLine
-            tableView.allowsSelection = true
-            tableView.reloadData()
-            noFavoritesLabel.isHidden = true
-        }
-        else {
-            tableView.isHidden = true
-            tableView.isScrollEnabled = false
-            noFavoritesLabel.isHidden = false
-        }
+    //MARK: Loading Data
+    func loadingStarted(){
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        activityIndicator.center = self.view.center
+        activityIndicator.style = .gray
+        activityIndicator.isHidden = false
+        view.addSubview(activityIndicator)
+        tableView.isHidden = true
+        tableView.isScrollEnabled = false
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = false
+        self.noFavoritesLabel.isHidden = true
     }
     
     //MARK: - Navigation
