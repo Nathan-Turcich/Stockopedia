6d91a4b8ee065da6f51b15cf9be1a141865c786d
Nathan Turcich <nmt2@illinois.edu>
2019-04-01 12:15:26 -0500

    Added Two Views

---
+++ Stockopedia/Profile/BuySellViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BuySellViewController.swift
+//  Stockopedia
+//
+//  Created by Nathan Turcich on 4/1/19.
+//  Copyright © 2019 Joey Chung. All rights reserved.
+//
+
+import UIKit
+
+class BuySellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Buy or Sell Stocks"
+    }
+}
---
+++ Stockopedia/Profile/RecommendationsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendationsViewController.swift
+//  Stockopedia
+//
+//  Created by Nathan Turcich on 4/1/19.
+//  Copyright © 2019 Joey Chung. All rights reserved.
+//
+
+import UIKit
+
+class RecommendationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Stock Recommendations"
+    }
+}
