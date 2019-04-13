e3f2eabfc2f58b21d9306226e58357c072e4c8ee
Joey <jnchung2@illinois.edu>
2019-04-13 15:37:15 -0500

    Bug Fixes Again

--- Stockopedia/Back End/dailyUpdateScaper.py
+++ Stockopedia/Back End/dailyUpdateScaper.py
@@ -119,43 +119,59 @@ def scrapeWebsitesForRealTimeData(listOfURLs):
             abbr = ""
             fullName = ""
             
-            company_text = company.get_text()
-            begin = 0
-            end = 0
-            for x in range(len(company_text)):
-                if(company_text[x] == '('):
-                    begin = x + 1
-                if(company_text[x] == ')'):
-                    end = x
-                    abbr = company_text[begin:end]
+            if(company is not None):
+                company_text = company.get_text()
+                    begin = 0
+                        end = 0
+                for x in range(len(company_text)):
+                    if(company_text[x] == '('):
+                        begin = x + 1
+                    if(company_text[x] == ')'):
+                        end = x
+                        abbr = company_text[begin:end]
                 
-            fullName = company_text[:begin - 2]
+                fullName = company_text[:begin - 2]
             
             date = getCurrentTime()
-            
+
             open = html.find('span', attrs={"data-reactid": "57"})
+            if(open is not None):
+                open = open.get_text()
+
             close = html.find('span', attrs={"data-reactid": "41"})
+            if(close is not None):
+                close = close.get_text()
+    
 
             low_high = html.find('td', attrs={"data-reactid": "60"})
+            if(low_high is not None):
+                range_text = low_high.get_text()
+                begin = 0
+                end = 0
+                amount = 0
+                for x in range(len(range_text)):
+                    if(range_text[x] == ' ' and amount == 0):
+                        begin = x
+                        amount += 1
+                    if(range_text[x] == ' ' and amount == 1):
+                        end = x
+                            amount += 1
             
-            range_text = low_high.get_text()
-            begin = 0
-            end = 0
-            amount = 0
-            for x in range(len(range_text)):
-                if(range_text[x] == ' ' and amount == 0):
-                    begin = x
-                    amount += 1
-                if(range_text[x] == ' ' and amount == 1):
-                    end = x
-                    amount += 1
-            
-            low = range_text[0: begin]
-            high = range_text[end: len(range_text)]
+                low = range_text[0: begin]
+                high = range_text[end: len(range_text)]
+
 
             volume = html.find('span', attrs={"data-reactid": "69"})
+            if(volume is not None):
+                volume = volume.get_text()
+
             mrktcap = html.find('span', attrs={"data-reactid": "82"})
+            if(mrktcap is not None):
+                mrktcap = mrktcap.get_text()
+
             diff = html.find('span', attrs={"data-reactid": "35"})
+            if(diff is not None):
+                diff = diff.get_text()
 
             print(abbr)
             
