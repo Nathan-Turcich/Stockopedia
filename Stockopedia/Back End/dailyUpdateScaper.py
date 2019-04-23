80584d0674ffceb7f3634ecec0d2fb5b0e7f8849
Nathan Turcich <nmt2@illinois.edu>
2019-04-23 14:47:48 -0500

    Added Randomization to Scraper

--- Stockopedia/Back End/dailyUpdateScaper.py
+++ Stockopedia/Back End/dailyUpdateScaper.py
@@ -115,6 +115,10 @@ def scrapeWebsitesForTopics(listOfURLs):
                 symbol = url[begin:]
                 deleteNames.append(symbol)
                 print("DELETE: " + symbol)
+
+                # Random sleeps to decrease chance of being black listed with webscraper
+                randomizeLoop()
+                
     return topics, deleteNames
 
 # Function that will insert all the topics previously collected and put them in to the DB
